40fdd6d562678a2c85dea3c177939d24ed5a122f
cikavladimir <cikavladimir@gmail.com>
2020-08-19 21:42:54 -0400

    Create connect.py

---
+++ hive/connect.py
@@ -0,0 +1,19 @@
+### Installation:
+pip install pyhive
+pip install thrift-sasl
+
+### Connect:
+from pyhive import hive
+import pandas
+
+conn = hive.Connection(host='3.236.56.144', port=10000,  database='default')
+
+sql = 'select name, count(*) from names group by name'
+
+cursor = conn.cursor()
+cursor.execute('SELECT * from names')
+print (cursor.fetchall())
+
+
+df = pandas.read_sql(sql, conn)
+print (df)
