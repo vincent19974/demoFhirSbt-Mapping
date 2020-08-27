ef0d077d1f7f8989f4d79a9e3ede079312f0cc99
Nikola Roganovic <46172820+NikolaRoganovic@users.noreply.github.com>
2020-08-27 18:02:12 +0200

    Update aws-cli-emr.sh

--- emr/aws-cli-emr.sh
+++ emr/aws-cli-emr.sh
@@ -1,1 +1,5 @@
 aws emr create-cluster --name "TerminationProtectedCluster" --release-label emr-6.1.0  --applications Name=Hadoop Name=Hive Name=Pig Name=Tez Name=Hue  Name=JupyterHub Name=Spark   --ec2-attributes InstanceProfile=ec2_role_ssm,KeyName=emrkey --instance-type m1.large  --instance-count 1 --termination-protected  --service-role EMR_DefaultRole --ebs-root-volume-size 30 --log-uri s3://emr-TerminationProtectedCluster/
+
+
+Livy+Zepelin:
+aws emr create-cluster --name "ZeppelinEMR" --release-label emr-6.1.0 --applications Name=Hadoop Name=Hive Name=Pig Name=Tez Name=Hue Name=JupyterHub Name=Spark Name=Livy Name=Zeppelin --ec2-attributes InstanceProfile=ec2_role_ssm,KeyName=emrkey --instance-type m1.large --instance-count 1 --termination-protected --service-role EMR_DefaultRole --ebs-root-volume-size 30 --log-uri s3://emr-TerminationProtectedCluster/
