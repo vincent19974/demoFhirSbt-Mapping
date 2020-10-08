e77595edf5d6fb6e9d32cea7611808e4ed42bd33
SasaJovanovic-git <sasa.c.jovanovic@gmail.com>
2020-10-08 17:04:41 +0200

    Update skripta.sh

--- hive/skripta.sh
+++ hive/skripta.sh
@@ -1,7 +1,7 @@
 echo "The beginning of the script!" 
 
 echo "Copy a file from one S3 bucket to another S3 bucket." 
-
+aws s3 cp s3://sasabucket1/proba3.hql  proba3.hql
 currentDate = $(date +%F -r proba3.hql) 
 
 for (( ; ; )) 
@@ -13,13 +13,15 @@ do
        then 
                echo "File date changed!" 
                
-               sleep 5s 
+               
                hive -f proba3.hql 
                echo "SQL file started!" 
                /home/ec2-user/skripta.sh > hive_output.txt 
                aws s3 cp hive_output.txt s3://sasabucket1/ 
+               sleep 1m
        else 
                echo "The file is not new yet!"
+               sleep 1m 
 
 done 
 
