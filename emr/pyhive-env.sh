ff84e165a130e20beaedfd3d4dfda515dcd212c3
Marko Sulaja <48557209+BataKole@users.noreply.github.com>
2020-10-23 19:13:12 +0200

    Add files via upload
    shell/bash scrip to install prerequisites for python/hive apps.

---
+++ emr/pyhive-env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+pip install pandas
+
+pip install pyhive
+
+pip install wheel
+
+pip install thrift
+
+yum install gcc-c++ python-devel.x86_64 cyrus-sasl-devel.x86_64
+
+pip install thrift-sasl
