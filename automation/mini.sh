f6e1cecca7b8f152938ce5cf7b64aee893f9ded3
NikolaRoganovic <dzonixd@gmail.com>
2020-10-27 22:04:59 +0100

    updated

--- automation/mini.sh
+++ automation/mini.sh
@@ -1,5 +1,6 @@
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p $HOME/miniconda
 conda init bash
-eval "$(/home/hadoop/miniconda/bin/conda shell.bash hook)"
+eval "$(/root/miniconda/bin/conda shell.bash hook)"
+
 
--- automation/rec.sh
+++ automation/rec.sh
@@ -1,17 +1,17 @@
-conda create --name py38 python=3.8 
+conda create --name py38 python=3.8  -y
 
-conda activate py38
+conda activate py38 
 
-pip3 install pandas
+pip3 install pandas 
 
-pip3 install pyhive
+pip3 install pyhive 
 
-pip3 install wheel
+pip3 install wheel 
 
-pip3 install thrift
+pip3 install thrift 
 
-sudo yum install python3-devel
+sudo yum install python3-devel -y
 
-sudo yum install gcc-c++ python-devel.x86_64 cyrus-sasl-devel.x86_64
+sudo yum install gcc-c++ python-devel.x86_64 cyrus-sasl-devel.x86_64 -y
 
 pip3 install thrift-sasl
