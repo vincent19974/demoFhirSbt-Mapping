686f9c1ff17f2685a5db1766389bf0b82575c39c
cikavladimir <cikavladimir@gmail.com>
2020-10-30 08:09:58 -0400

    Create emr.sh

---
+++ emr/emr.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+~/miniconda3/bin/conda init bash
+~/miniconda3/bin/conda init zsh
+eval "$(/home/hadoop/miniconda3/bin/conda shell.bash hook)" \
+	&& conda create --name py38 python=3.8 -y \
+	&& conda init zsh \
+	&& eval "$(conda shell.bash hook)" \
+        && conda activate py38
+	
+	 
+pip3 install pandas
+pip3 install pyhive
+pip3 install wheel
+pip3 install thrift
+sudo yum install python3-devel -y
+sudo yum install python-pip gcc gcc-c++ python-virtualenv cyrus-sasl-devel -y
+pip3 install thrift-sasl
+
+pip install cassandra-driver  
+pip install boto3  
+
+
+
+# sudo su hadoop
+conda activate py38
+
+mkdir -p /tmp/test/
+chmod 777 -R /tmp/test/
+
+cd /tmp/test/
+aws s3 cp s3://p360-poc-s3-log-storage/test/script.py .
+
+echo "script will run" >> /tmp/test/logs.txt
+
+python3 /tmp/test/script.py
+
+echo "script ran good" >> /tmp/test/logs.txt
+
+
+
