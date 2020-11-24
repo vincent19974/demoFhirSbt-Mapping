7ef2377f70e8fa306e06841432c3a05925bb728c
SasaJovanovic-git <sasa.c.jovanovic@gmail.com>
2020-11-24 19:03:05 +0100

    Add files via upload

---
+++ Boto3/ListFiles.py
@@ -0,0 +1,18 @@
+import os
+import boto3
+from pprint import pprint
+
+
+s3 = boto3.resource('s3')
+my_bucket = s3.Bucket('sasabucket1')
+for my_bucket_object in my_bucket.objects.all():
+    pprint(my_bucket_object)
+
+
+s3 = boto3.client('s3')
+bucket = 'sasabucket1'
+prefix = 'hql-input/'
+for obj in s3.list_objects_v2(Bucket=bucket, Prefix=prefix)['Contents']:
+    pprint(obj['Key'])
+    pprint("==========================")
+
